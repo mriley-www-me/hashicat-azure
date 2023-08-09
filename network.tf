dcfd8fdf2e0b459e583539c4cca4ccc6a3848a59
mriley-www-me <141857166+mriley-www-me@users.noreply.github.com>
2023-08-09 14:51:40 -0500

    Update network.tf
    doin it 3 times

--- network.tf
+++ network.tf
@@ -3,6 +3,6 @@ module "network" {
   version = "3.5.0"
   resource_group_name=azurerm_resource_group.myresourcegroup.name
   tags = {
-    Department = "Billable"
+      Department = "Billable"
   }
 }

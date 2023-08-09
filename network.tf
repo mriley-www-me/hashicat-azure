3552232c4b8461eebe99a0b09d8c3599eb4335b5
mriley-www-me <141857166+mriley-www-me@users.noreply.github.com>
2023-08-09 14:48:40 -0500

    Update network.tf
    updating tag

--- network.tf
+++ network.tf
@@ -2,4 +2,7 @@ module "network" {
   source  = "app.terraform.io/mriley-training/network/azurerm"
   version = "3.5.0"
   resource_group_name=azurerm_resource_group.myresourcegroup.name
+  tags = {
+    Department = "Billable"
+  }
 }
